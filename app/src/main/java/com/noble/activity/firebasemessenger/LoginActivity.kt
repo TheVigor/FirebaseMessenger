f870b5e66dac96245811237d9b702d394a984653
TheVigor <igor.proskuryakov14@gmail.com>
2019-01-26 22:18:26 +0300

    added simple auth based on firebase auth

--- app/src/main/java/com/noble/activity/firebasemessenger/LoginActivity.kt
+++ app/src/main/java/com/noble/activity/firebasemessenger/LoginActivity.kt
@@ -2,12 +2,29 @@ package com.noble.activity.firebasemessenger
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        login_button.setOnClickListener {
+            val email = email_input.text.toString()
+            val password = password_input.text.toString()
+
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+
+            Log.d("LoginActivity", "$email $password")
+        }
+
+        back_to_registration_input.setOnClickListener {
+            finish()
+        }
+
     }
 
 }
--- app/src/main/java/com/noble/activity/firebasemessenger/MainActivity.kt
+++ app/src/main/java/com/noble/activity/firebasemessenger/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,11 +15,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         register_button.setOnClickListener {
-            val email = email_input.text.toString()
-            val password = password_input.text.toString()
-
-            Log.d("MainActivity", "Email is $email")
-            Log.d("MainActivity", "Password is $password")
+            performRegister()
         }
 
         already_have_account_input.setOnClickListener {
@@ -26,5 +24,29 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun performRegister() {
+        val email = email_input.text.toString()
+        val password = password_input.text.toString()
+
+        Log.d("MainActivity", "Email is $email")
+        Log.d("MainActivity", "Password is $password")
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Empty", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener{
+                if (!it.isSuccessful) return@addOnCompleteListener
+
+                Log.d("MainActivity", "Success:" + it.result?.user?.uid)
+            }
+            .addOnFailureListener{
+                Log.d("MainActivity", "Failed to create user: ${it.message}")
+                Toast.makeText(this, "Failed to create user: ${it.message}", Toast.LENGTH_LONG).show()
+            }
     }
 }
